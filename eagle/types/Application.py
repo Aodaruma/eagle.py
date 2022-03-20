89cb8325a8ec45520ba118822c620504447b74ce
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-03-20 15:24:30 +0900

    Update Application.py
    add get_info func

--- eagle/types/Application.py
+++ eagle/types/Application.py
@@ -24,3 +24,14 @@ class Application(object):
         self._execPath = None
         self._platform = None
         self._library = None
+
+    async def get_info(self):
+        """
+        Get information of the application of Eagle
+        """
+        d = await self._api.application_info()
+        self._version = d["version"]
+        self._prereleaseVersion = d["prereleaseVersion"]
+        self._buildVersion = d["buildVersion"]
+        self._execPath = d["execPath"]
+        self._platform = d["platform"]
