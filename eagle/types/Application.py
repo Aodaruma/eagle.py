3f348125ebdbaccdc727a37816ac2daa1109025f
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-03-20 15:24:21 +0900

    Update Application.py
    add __init__ func

--- eagle/types/Application.py
+++ eagle/types/Application.py
@@ -1,5 +1,26 @@
+from eagle.basic.api_connection import APIconnection
+from eagle.basic.cached_property import cached_property
+from eagle.types.Library import Library
+
+
 class Application(object):
     """
-    docstring
+    Application class of Eagle
     """
-    pass
+
+    def __init__(self, api_connection: APIconnection):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        api_connection : APIconnection
+            the instance of APIconnection
+        """
+        self._api = api_connection
+        self._version = None
+        self._prereleaseVersion = None
+        self._buildVersion = None
+        self._execPath = None
+        self._platform = None
+        self._library = None
